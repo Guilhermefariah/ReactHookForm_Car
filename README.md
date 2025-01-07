3a7fb238492833b7050fe3863fdebbfa8d9af6ad
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-01-07 17:23:12 -0300

    fix: form created with React hook validation form

--- README.md
+++ README.md
@@ -1,8 +1,54 @@
-# React + Vite
+# Seguradora de Veículos
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+**Resumo**
+-----------
 
-Currently, two official plugins are available:
+Este projeto é uma aplicação web que simula uma Seguradora de Veículos. O objetivo é fornecer uma ferramenta para calcular o valor da segurança de um veículo com base em sua idade e preço.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+**Funcionalidades**
+-------------------
+
+* Calcula o valor da segurança de um veículo com base em sua idade e preço
+* Considera a idade do veículo e o preço para calcular o valor da segurança
+* Oferece a opção de renovar a segurança com um desconto de 10%
+
+**Tecnologias Utilizadas**
+---------------------------
+
+* React
+* Vite
+* JavaScript
+* CSS
+* HTML
+
+**Instalação**
+--------------
+
+Para instalar o projeto, siga os passos abaixo:
+
+1. Clone o repositório: `git clone https://github.com/Guilhermefariah/ReactHookForm_Car.git`
+2. Instale as dependências: `npm install`
+3. Inicie o servidor de desenvolvimento: `npm run dev`
+
+**Uso**
+------
+
+1. Acesse a aplicação em `http://localhost:3000`
+2. Preencha os campos com as informações do veículo (modelo, preço e ano)
+3. Clique em "Calcular" para obter o valor da segurança
+4. Se desejar renovar a segurança, marque a opção "Renovar segurança" e clique em "Calcular" novamente
+
+**Contribuição**
+--------------
+
+Se você deseja contribuir para o projeto, por favor, siga os passos abaixo:
+
+1. Faça um fork do repositório
+2. Crie uma branch para sua feature ou correção
+3. Faça as alterações necessárias
+4. Envie um pull request para o repositório original
+
+**Licença**
+---------
+
+Este projeto é licenciado sob a licença MIT.
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
+import { Fragment, useState } from "react";
 import { useForm } from "react-hook-form";
 
 import "./App.css";
 
-function App() {
-  const { handleSubmit, register } = useForm();
+export default function App() {
+  const [currentVehicleSimulation, setCurrentVehicleSimulation] = useState({
+    securityValue: 0,
+    installmentsQuantity: 0,
+    individualInstallmentValue: 0,
+  });
+  const { handleSubmit, register, reset } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
+    const currentYear = new Date().getFullYear();
+    const vehicleYear = Number(data.vehicleYear);
+    const vehiclePrice = Number(data.vehiclePrice);
+
+    const vehicleAge = currentYear - vehicleYear;
+
+    let securityValue = 0;
+
+    if (vehicleAge <= 5) {
+      securityValue = vehiclePrice * (3 / 100);
+    } else {
+      securityValue = vehiclePrice * (4 / 100);
+    }
+
+    if (data.isSecurityRenovation) {
+      securityValue = securityValue * (90 / 100);
+    }
+
+    const installmentsQuantity = 12;
+
+    setCurrentVehicleSimulation({
+      securityValue: securityValue.toFixed(2),
+      installmentsQuantity,
+      individualInstallmentValue: Intl.NumberFormat("pt-BR").format(
+        (securityValue / installmentsQuantity).toFixed(2)
+      ),
+    });
+  };
+
+  const handleResetForm = () => {
+    reset({ vehicleModel: "", vehiclePrice: "", vehicleYear: "" });
+    setCurrentVehicleSimulation({
+      securityValue: 0,
+      installmentsQuantity: 0,
+      individualInstallmentValue: 0,
+    });
   };
 
   return (
-    <>
+    <Fragment>
       <nav className="navbar bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
@@ -28,11 +69,7 @@ function App() {
 
       <div className="card text-center container my-auto p-0">
         <div className="card-header p-4">
-          <img
-            src="logo.png"
-            alt="Seguradora de Veículos"
-            className="logo"
-          />
+          <img src="logo.png" alt="Seguradora de Veículos" className="logo" />
           <h1 className="h2 my-5">Seguradora de Veículos</h1>
         </div>
 
@@ -52,7 +89,7 @@ function App() {
               <div className="col mb-3">
                 <input
                   type="number"
-                  placeholder="R$ Avaliação (FIPE)"
+                  placeholder="R$ Avaliação (Fipe)"
                   className="form-control"
                   {...register("vehiclePrice", { required: true })}
                 />
@@ -74,7 +111,7 @@ function App() {
                   type="checkbox"
                   className="form-check-input"
                   id="renovation-checkbox"
-                  {...register('isSecurityRenovation')}
+                  {...register("isSecurityRenovation")}
                 />
                 <label
                   className="form-check-label"
@@ -91,16 +128,30 @@ function App() {
               </button>
 
               <button
+                onClick={handleResetForm}
                 type="button"
-                className="btn btn-danger col ms-3">
-                  Limpar
-                </button>
+                className="btn btn-danger col ms-3"
+              >
+                Limpar
+              </button>
             </div>
           </form>
         </div>
+
+        <div className="card-footer text-body-secondary py-4 row">
+          {currentVehicleSimulation.securityValue > 0 && (
+            <Fragment>
+              <p className="col text-primary">
+                R$ Seguro: {currentVehicleSimulation.securityValue}
+              </p>
+              <p className="col text-primary">
+                Em {currentVehicleSimulation.installmentsQuantity}x de R${" "}
+                {currentVehicleSimulation.individualInstallmentValue}
+              </p>
+            </Fragment>
+          )}
+        </div>
       </div>
-    </>
+    </Fragment>
   );
 }
-
-export default App;
